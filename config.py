fe2df6117255e1fccfad37f826d81d7de2ba3edc
Unknown <chen24296@gmail.com>
2018-10-19 15:12:27 +0800

    web_bbs

---
+++ config.py
@@ -0,0 +1,5 @@
+import os.path
+
+secret_key = 'web bbs'
+user_file_director = r'{}/static/user_image/'.format(os.path.dirname(__file__))
+accept_user_file_type = ['jpg', 'gif', 'png']
---
+++ models/reply.py
@@ -0,0 +1,39 @@
+# import time
+# from models import Model
+from models.mongo import Mongo
+
+
+# class Reply(Model):
+#     def __init__(self, form):
+#         self.id = None
+#         self.content = form.get('content', '')
+#         self.ct = int(time.time())
+#         self.ut = self.ct
+#         self.topic_id = int(form.get('topic_id', -1))
+#         self.user_id = -1
+#
+#     def user(self):
+#         from .user import User
+#         u = User.find(self.user_id)
+#         return u
+#
+#     def set_user_id(self, user_id):
+#         self.user_id = user_id
+#         self.save()
+
+class Reply(Mongo):
+    __fields__ = Mongo.__fields__ + [
+        ('content', str, ''),
+        ('topic_id', int, -1),
+        ('receiver_id', int, -1),
+        ('user_id', int, -1)
+    ]
+
+    def user(self):
+        from .user import User
+        u = User.find(self.user_id)
+        return u
+
+    def set_user_id(self, user_id):
+        self.user_id = user_id
+        self.save()
